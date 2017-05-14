d6c48b0e65b88c9f91e28e081fd10f4d965a7479
Matthew Ferry <hello@matthew-ferry.com>
2017-05-14 12:41:13 -0700

    Adds textConfig JSON for easier baseline variable creation
    Author:    Matthew Ferry <hello@matthew-ferry.com>

--- plugins/fontsize-calc.js
+++ plugins/fontsize-calc.js
@@ -1,19 +1,33 @@
-const rootSize = 12;
-const bodySize = 16;
-const scaleSize = 16;
-const scaleRatio = 1.333333;
-const scaleIncrement = 0;
-const fontSize = scaleSize * Math.pow(scaleRatio, scaleIncrement);
-const lineHeight = 1;
-const capHeight = 0.705;
-const bottomSpace = 0;
-const baselineShift = fontSize / 2 * ((lineHeight * rootSize / fontSize) - capHeight) / rootSize + .00001;
-const rhythmReset = bottomSpace - baselineShift;
-const typeCss = `
-  font-size: ${fontSize/rootSize}rem;
-  line-height: ${lineHeight}rem;
-  margin: 0 0 ${rhythmReset}rem;
-  padding-top: ${baselineShift}rem;
+const fs = require('fs');
+const textConfig = require('../textConfig.json');
+const rootSize = textConfig.gridUnit;
+const scaleBaseSize = textConfig.scaleBaseSize;
+const bodyFamily = textConfig.fontFamily.toString();
+const scaleRatio = textConfig.scaleRatio;
+const capHeight = textConfig.capHeight;
+
+let variableSheet = `
+  --root-font-size: ${rootSize / 16 * 100}%;
+  --body-font-family: ${bodyFamily};
+`;
+
+textConfig.sizes.forEach((size) => {
+  const fontSize = scaleBaseSize * Math.pow(scaleRatio, size.scale);
+  const lineHeight = size.hasOwnProperty("lineHeight") ? (
+    Math.ceil(size.lineHeight * fontSize / rootSize)
+  ) : (
+    Math.ceil((fontSize + .001) / rootSize)
+  );
+  const baselineShift = ((lineHeight * rootSize / fontSize) - capHeight) / 2;
+  const typeVariables = `
+  --${size.element}-font-size: ${fontSize / rootSize}rem; /* ~${Math.round(fontSize)}px (${scaleRatio}^${size.scale} @${scaleBaseSize}) */
+  --${size.element}-line-height: ${lineHeight}rem; /* ~${Math.round(lineHeight * rootSize / fontSize * 10000) / 10000} */
+  --${size.element}-baseline-offset: ${baselineShift * fontSize / rootSize}rem;
+  --${size.element}-margin-bottom: ${size.bottom};
+  --${size.element}-padding-top: ${size.top};
   `;
 
-console.log(typeCss);
+  variableSheet += typeVariables;
+});
+
+fs.writeFile('lib/theme/_text.css', ':root {' + variableSheet + '}');
