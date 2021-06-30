23d28fba92e356c7a2957462df90af0e5ae61d4a
Matthew Ferry <matthewferry@github.com>
2021-06-30 09:27:24 -0600

    Updates

--- helpers/fontsize-calc.js
+++ helpers/fontsize-calc.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const textConfig = require('../textConfig.json');
-const rootSize = textConfig.gridUnit;
+const rootSize = textConfig.rootSize;
+const gridUnit = textConfig.gridUnit;
 const scaleBaseSize = textConfig.scaleBaseSize;
 const bodyFamily = textConfig.fontFamily.join(', ');
 const scaleRatio = textConfig.scaleRatio;
@@ -11,21 +12,20 @@ let variableSheet = `
   --body-font-family: ${bodyFamily};`;
 
 textConfig.sizes.forEach((size) => {
-  const fontSize = scaleBaseSize * Math.pow(scaleRatio, size.scale);
+  const fontSize = scaleBaseSize * Math.pow(scaleRatio, size.scale); // Returns pixel size
   const lineHeight = size.hasOwnProperty("lineHeight") ? (
-    Math.ceil(size.lineHeight * fontSize / rootSize)
+    Math.round(size.lineHeight * fontSize / rootSize * 2) / 2 // Returns rem unit
   ) : (
     Math.ceil((fontSize + .001) / rootSize)
   );
-  const baselineShift = ((lineHeight * rootSize / fontSize) - capHeight) / 2;
+  // const baselineShift = ((lineHeight * rootSize / fontSize) - capHeight) / 2;
   const typeVariables = `
   --${size.element}-font-size: ${fontSize / rootSize}rem; /* ~${Math.round(fontSize)}px (${scaleRatio}^${size.scale} @${scaleBaseSize}) */
-  --${size.element}-line-height: ${lineHeight}rem; /* ~${Math.round(lineHeight * rootSize / fontSize * 10000) / 10000} */
-  --${size.element}-baseline-offset: ${baselineShift * fontSize / rootSize}rem;
-  --${size.element}-margin-bottom: ${size.bottom};
-  --${size.element}-padding-top: ${size.top};`;
-
+  --${size.element}-line-height: ${lineHeight}rem; /* ~${Math.round(lineHeight * rootSize / fontSize * 10000) / 10000} */`;
+  // --${size.element}-baseline-offset: ${baselineShift * fontSize / rootSize}rem;
+  // --${size.element}-margin-bottom: ${size.bottom};
+  // --${size.element}-padding-top: ${size.top};
   variableSheet += typeVariables;
-});
+}, () => true);
 
-fs.writeFile('lib/theme/_text.css', ':root {' + variableSheet + '\n}\n');
+fs.writeFile('src/theme/_text.css', ':root {' + variableSheet + '\n}\n', () => true);
